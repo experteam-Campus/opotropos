b7f7a5725e8ea543d28075041aac4d3bcb61e28e
experteam-Campus <89127414+experteam-Campus@users.noreply.github.com>
2023-01-18 08:52:11 +0200

    Add files via upload

---
+++ opotropos_js.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
